a416ee77a69eaec87915d9d1a1e4dbd43a81da54
pj8912 <jpintofun2b@gmail.com>
2022-04-22 20:35:59 +0530

    fixed minor bugs major functionalities completed

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@ function main_header($title = 'document')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
-<body>
+<body style="background:whitesmoke">
 <nav class="navbar navbar-expand-lg  navbar-light bg-light">
 <div class="container-fluid">
     <a href="http://localhost/php_bug_tracking/" class="nav-brand h4 ">
